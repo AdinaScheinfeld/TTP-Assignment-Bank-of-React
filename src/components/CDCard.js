ae0beb8aa4768964d47876fefcd2a4c012b6f3bb
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-20 12:48:44 -0500

    adds styling

--- src/components/CDCard.js
+++ src/components/CDCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CDCard.css';
 
 const CDCard = ( props ) => {
     return ( 
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { UserNav } from './index';
 import { CDCard } from './index';
+import './CD.css';
 
 class Debits extends Component {
 
@@ -40,43 +41,46 @@ class Debits extends Component {
         return ( 
             <div className='container'>
                 <UserNav />
-                <h1>Debits</h1>  
-                <h2>Balance: { this.props.accountBalance.toFixed(2) }</h2>
+                <div className='mainContainer'>
+                    <h1>Debits</h1>  
+                    <h2>Balance: { this.props.accountBalance.toFixed(2) }</h2>
 
-                <div className='add'>
-                    <form onSubmit={ this.addDebit }>
-                        <p>Add a new debit: </p>
-                        <label>Description: </label>
-                        <input 
-                            type='text' 
-                            name='description' 
-                            placeholder='e.g. Tuition' 
-                            onChange={ this.getDebitDescription } 
-                            value={ this.state.newDebitDescription }
-                        />
-                        <label>Amount: </label>
-                        <input 
-                            type='text' 
-                            name='amount' 
-                            placeholder='e.g. 3000' 
-                            onChange={ this.getDebitCost } 
-                            value={ this.newDebitCost }
-                        />
-                        <button>Add Debit</button>                
-                    </form>
-                </div>
+                    <div className='add'>
+                        <form onSubmit={ this.addDebit }>
+                            <p>Add a new debit: </p>
+                            <label>Description: </label>
+                            <input 
+                                type='text' 
+                                name='description' 
+                                placeholder='e.g. Tuition' 
+                                onChange={ this.getDebitDescription } 
+                                value={ this.state.newDebitDescription }
+                            />
+                            <label>Amount: </label>
+                            <input 
+                                type='text' 
+                                name='amount' 
+                                placeholder='e.g. 3000' 
+                                onChange={ this.getDebitCost } 
+                                value={ this.newDebitCost }
+                            />
+                            <button>Add Debit</button>                
+                        </form>
+                    </div>
 
-                <div className='debitCards'>
-                    { this.props.currentDebits.map((item, index) => (
-                    <CDCard 
-                        key={ index } 
-                        description={ item.description } 
-                        amount={ item.amount } 
-                        date={ new Date(item.date).toLocaleString() } 
-                    />
-                ))}                 
+                    <div className='debitCards'>
+                        { this.props.currentDebits.map((item, index) => (
+                        <CDCard 
+                            key={ index } 
+                            description={ item.description } 
+                            amount={ item.amount } 
+                            date={ new Date(item.date).toLocaleString() } 
+                        />
+                    ))}                 
+                    </div>
                 </div>
             </div>
+
 
          );
     }
